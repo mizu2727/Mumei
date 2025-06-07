e04aba42611f7b112e6e22d485255606a1b9044b
mizu2727 <m98340707@gmail.com>
2025-06-07 14:16:47 +0900

    ★実装したもの 　ドア開閉時の効果音を実装
    ★廃止予定
    　・Inventory.cs
    　・EnemyCollisionHandler.cs
    
    ★課題
    　①敵生成地点オブジェクト(空のプレハブ)を複数個用意し、
    　　敵の生成地点をランダムに決める。
    　②プレイヤーのアニメーションで、
    　　移動→停止の際に、
    　　停止状態でも移動のアニメーションが少しだけ再生されてしまう。
    　　停止した状態でダッシュボタン→移動ボタンの順に同時押しして移動すると、
    　　ダッシュアニメーションが再生されない。
    　　
    　③ゲームの見た目関係を整える
    　④ロード画面追加
    　⑤ミステリーアイテムを間違えた際の処理を追加
    　⑥BGM・SE追加
    　⑦難易度調整
    　⑧C#ファイルのリファクタリングが必要
    　⑨アイテムの外枠部分を光らせたい
    
    ★今後の作業方針
    　■ステージマップ制作方針
    　　変更前：マップをランダムに生成後、敵・アイテム・ゴール・徘徊地点を生成。
    　　また、プレイヤーもランダムにワープさせる
    　　
    　　変更後：ステージプレハブをn * n でランダムに生成。
    　　　敵・アイテムをそれぞれランダムな生成地点へと生成させる。(生成地点は手動で設定する)
    　　　徘徊地点・ゴール・プレイヤーのワープ位置は手動。
    　　
    　■不明点をAIの活用で解決する場合、下記AIを使用してみる
    　　・XのGrok
    　　・Gemini
    　　
    　　
    ★備考欄
    　■DontDestroyOnLoad(gameObject)を記載すると、
    　　そのオブジェクトがシーン移動する際に、
    　　シーン移動前の座標を保持した状態になる。
    　　
    　■敵とプレイヤー両方にカプセルコライダーを付けて、
    　　is Triggerを両方ともオンすることで、
    　　ゲームオーバー画面へ遷移できた。
    　　
    　■Collider等の当たり判定関係は、
    　　基本的に動的オブジェクトにRigidbodyまたはCharacterControllerをアタッチする必要がある
    　　
    　■課題：オブジェクトを強調させる処理について
    　　Unity6とOutline toolkit (URP)のバージョンの互換性がないため、
    　　テストプレイ時に下記エラーが発生する。強調処理は動作している。
    Non-matching Profiler.EndSample (Every EndSample call must have a preceding BeginSample call within the same frame): OutlinePass
    Previous 5 samples:
     Graphics.DrawProcedural
     RenderingCommandBuffer.PrepareKeywords
     RenderTarget.SetActive
     Graphics.DrawProcedural
     RenderingCommandBuffer.PrepareKeywords
    In the scope:
     Outline-2048
     ScriptableRenderer.Execute: PC_Renderer
     UniversalRenderPipeline.RenderSingleCameraInternal: Main Camera
     Inl_ScriptableRenderContext.Submit
     Inl_RenderCameraStack
    
    Missing Profiler.EndSample (Every BeginSample call must have a subsequent EndSample call within the same frame): OutlinePass
    Previous 5 samples:
     RenderTarget.SetActive
     OutlinePass
     Outline-2048
     RenderLoop.CleanupNodeQueue
     WaitForCompleted
    In the scope:
     OutlinePass
     Outline-2048
     ScriptableRenderer.Execute: PC_Renderer
     UniversalRenderPipeline.RenderSingleCameraInternal: Main Camera
     Inl_ScriptableRenderContext.Submit

--- Assets/Originals/Scripts/MainSence/Objects/Others/Door.cs
+++ Assets/Originals/Scripts/MainSence/Objects/Others/Door.cs
@@ -1,14 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Door : MonoBehaviour
 {
-    public bool isOpenDoor = false;//ドアの開閉の判定
-    [SerializeField] public bool isNeedKeyDoor = false;//鍵が掛かっているかを判定
+
+    [Header("ドアの開閉の判定")]
+    public bool isOpenDoor = false;
+
+    [Header("鍵が掛かっているかを判定")]
+    [SerializeField] public bool isNeedKeyDoor = false;
+
+    [Header("ドア開閉時の回転角度")]
     [SerializeField] float openDirenctionValue = 90.0f;//ドアを開ける角度
     [SerializeField] float closeDirenctionValue = 0.0f;//ドアを閉じる角度
-    
-    private Player player;//プレイヤー
+
+    //プレイヤー
+    private Player player;
+
+    [Header("ドア間との距離を測定したいオブジェクトをアタッチ(ヒエラルキー上のプレイヤーをアタッチすること)")]
+    [SerializeField] public Transform targetPoint;
+
+
+    [Header("サウンド関連")]
+    private AudioSource audioSourceSE;//Door専用のAudioSource
+    [SerializeField] private AudioClip openSE;
+    [SerializeField] private AudioClip closeSE;
+
+    [Header("サウンドの距離関連(要調整)")]
+    [SerializeField] private float maxSoundDistance = 10f; // 音量が最大になる距離
+    [SerializeField] private float minSoundDistance = 20f; // 音量が最小になる距離
+    [SerializeField] private float maxVolume = 1.0f; // 最大音量
+    [SerializeField] private float minVolume = 0.0f; // 最小音量
+
+    private void Start()
+    {
+        audioSourceSE = MusicController.Instance.GetAudioSource();
+    }
 
     public void DoorSystem() 
     {
@@ -41,6 +69,7 @@ public class Door : MonoBehaviour
     {
         isOpenDoor = true;
         transform.Rotate(0, openDirenctionValue, 0);
+        DoorSE();
     }
 
     //ドアを閉める
@@ -48,5 +77,47 @@ public class Door : MonoBehaviour
     {
         isOpenDoor = false;
         transform.Rotate(0, closeDirenctionValue, 0);
+        DoorSE();
+    }
+
+
+    //ドアの開閉の効果音
+    void DoorSE() 
+    {
+        // 効果音制御
+        AudioClip currentSE = (isOpenDoor) ? openSE : closeSE;
+
+        // プレイヤーとの距離を測定
+        float distance = Vector3.Distance(transform.position, targetPoint.position);
+
+        // 距離に基づく音量計算
+        float volume = CalculateVolumeBasedOnDistance(distance);
+
+        MusicController.Instance.PlayAudioSE(audioSourceSE, currentSE);
+
+        //音量を設定
+        audioSourceSE.volume = volume;
+    }
+
+
+    // 距離に基づく音量を計算するメソッド
+    private float CalculateVolumeBasedOnDistance(float distance)
+    {
+        if (distance <= maxSoundDistance)
+        {
+            // 最大音量
+            return maxVolume; 
+        }
+        else if (distance >= minSoundDistance)
+        {
+            // 最小音量
+            return minVolume; 
+        }
+        else
+        {
+            // 距離に基づいて線形補間
+            float t = (distance - maxSoundDistance) / (minSoundDistance - maxSoundDistance);
+            return Mathf.Lerp(maxVolume, minVolume, t);
+        }
     }
 }
